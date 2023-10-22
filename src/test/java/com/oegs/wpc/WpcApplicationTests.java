52e7c38f8a5eec2aa52844fc2796aba735cfbdb0
Tolu96 <37343265+Tolu96@users.noreply.github.com>
2023-10-22 21:04:34 +0200

    First Commit

---
+++ src/test/java/com/oegs/wpc/WpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oegs.wpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
