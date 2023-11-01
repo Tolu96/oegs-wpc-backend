9359811c5c523b1e2b505f865054317b63cfd895
Tolu96 <37343265+Tolu96@users.noreply.github.com>
2023-11-01 21:16:14 +0100

    Created Docker compose File, connected to database and made minor changes in Employee and dependencies

--- src/main/java/com/oegs/wpc/Employee/Employee.java
+++ src/main/java/com/oegs/wpc/Employee/Employee.java
@@ -4,6 +4,7 @@ package com.oegs.wpc.Employee;
 import com.oegs.wpc.enums.GenderEnum;
 import com.oegs.wpc.enums.NationalityEnum;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -20,16 +21,26 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID employeeId;
+    @NotNull
     private String firstName;
+    @NotNull
     private String lastName;
+    @NotNull
     private String address;
+    @NotNull
     private int postCode;
+    @NotNull
     private String town;
+    @NotNull
     private GenderEnum genderEnum;
+    @NotNull
     private NationalityEnum nationality;
+    @NotNull
     private LocalDate employedSince;
     private boolean isEmployed;
+    @NotNull
     private final LocalDateTime createdAt = LocalDateTime.now();
+    @NotNull
     private LocalDateTime updatedAt;
 
     public Employee(UUID employeeId, String firstName, String lastName, String address, int postCode, String town,
